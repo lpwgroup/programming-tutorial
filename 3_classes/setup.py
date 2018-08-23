7e6c114e18ce3861e3055732c252bd99b9c0f5b5
qyd <saviorbruce@gmail.com>
2018-08-23 11:43:45 -0700

    use find_packages function

--- 3_classes/setup.py
+++ 3_classes/setup.py
@@ -2,7 +2,7 @@
 mdsim
 A Simple Molecular Dynamics Simulation Package
 """
-from setuptools import setup
+from setuptools import setup, find_packages
 
 DOCLINES = __doc__.split("\n")
 
@@ -12,7 +12,7 @@ setup(
     description=DOCLINES[0],
     long_description="\n".join(DOCLINES[2:]),
     version=0.1,
-    packages=['mdsim'],
+    packages=find_packages(),
     url='https://github.com/lpwgroup/programming-tutorial/',
     install_requires=[
         'numpy>=1.11',
